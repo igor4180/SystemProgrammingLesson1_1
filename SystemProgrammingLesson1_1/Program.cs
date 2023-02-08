b93a17660f401dcbad585e6892317e05d1d55dc8
--- <---.mainry>
2023-02-08 20:44:14 +0300

    78

--- SystemProgrammingLesson1_1/Program.cs
+++ SystemProgrammingLesson1_1/Program.cs
@@ -11,11 +11,37 @@ namespace CSharpSPLesson1
 		const uint MB_ICONWARNING = 0x030;
 		const uint MB_CANCELTRYCONTINUE = 0x06;
 		const uint MB_DEFBUTTON2 = 0x0100;
+		const uint MB_YESNOCANCEL = 0x00000003;
+
+		static int MyMessageBox(int number)
+		{
+			return MessageBox(IntPtr.Zero, "Yes - число угадано верно, " + 
+				"No - загаданное число меньше, " + "Cancel - загаданное число больше \n Может быть вы загадали " + number.ToString(), "Угадай число ", MB_YESNOCANCEL | MB_DEFBUTTON2);
+		}
 
 		static void Main(string[] args)
 		{
-			MessageBox(IntPtr.Zero, "Hello word", "Title", MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2);
-
+			//MessageBox(IntPtr.Zero, "Hello word", "Title", MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2);
+			int minNumber = 0;
+			int maxNumber = 100;
+			Random random = new Random();
+			int result = random.Next(minNumber, maxNumber);
+			int answer = 0;
+			while ((answer = MyMessageBox(result)) != 6)
+			{
+				if (answer == 2)
+				{
+					minNumber = result;
+					result = random.Next(minNumber, maxNumber);
+				}
+				if (answer == 7)
+				{
+					maxNumber = result;
+					result = random.Next(minNumber, maxNumber);
+				}
+			}
+			MessageBox(IntPtr.Zero, "Ура, я прочитал ваши мысли! Число " + result.ToString(),
+				"Число угадано", 0x0);
 			//Process process = new Process();
 			//process.StartInfo = new ProcessStartInfo("notepad.exe");
 			//process.Start();
